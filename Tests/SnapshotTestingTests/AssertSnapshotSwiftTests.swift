2d9666a3b93fd884dbfc6f8fd3198853f2792610
Claude Houle <claudehoule@icloud.com>
2026-07-18 20:10:21 -0400

    fix: remove duplicate MainActor dump test (#61)

--- Tests/SnapshotTestingTests/AssertSnapshotSwiftTests.swift
+++ Tests/SnapshotTestingTests/AssertSnapshotSwiftTests.swift
@@ -11,13 +11,5 @@ extension BaseSuite {
             assertSnapshot(of: user, as: .dump)
         }
     }
-
-    @MainActor struct MainActorTests {
-        @Test func dump() {
-            struct User { let id: Int, name: String, bio: String }
-            let user = User(id: 1, name: "Blobby", bio: "Blobbed around the world.")
-            assertSnapshot(of: user, as: .dump)
-        }
-    }
 }
 #endif
